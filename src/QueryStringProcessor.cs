b57d0bc2ab5d6f4c0b216fff8e59ab1939b8380e
Jesse <jesse@jesseh.net>
2016-11-06 23:33:25 -0800

    add projects, initial class and first test

---
+++ src/QueryStringProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace UrlFilter
+{
+    public static class QueryString<T> where T : class
+    {        
+        public static Expression Process(string queryString)
+        {
+            var segments = queryString.Split(' ');
+            var paramExpression = Expression.Parameter(typeof(T));
+            var left = Expression.Property(paramExpression, typeof(T).GetRuntimeProperty(segments[0]));
+            var right = Expression.Constant(segments[2]);
+            return Expression.Equal(left, right);
+        }
+    }
+}
---
+++ tests/UrlFilter.Tests/QueryStringTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlFilter.Tests
+{
+    [TestFixture]
+    public class QueryStringTests
+    {
+        [Test]
+        public void should_match_one_document_with_eq_expression()
+        {
+            var expectedValue = 3;
+            var queryString = $"value eq {expectedValue}";
+            var testDocs = GetTestDocuments(10);
+            
+            var query = QueryString<TestDocument>.Process(queryString);
+            var result = testDocs.Provider.CreateQuery<TestDocument>(query);
+
+            foreach (var doc in result)
+            {
+                doc.Value.Should().Be(expectedValue);
+            }
+        }
+
+        private IQueryable<TestDocument> GetTestDocuments(int quantity)
+        {
+            return Enumerable.Range(0, quantity)
+                .Select(x => new TestDocument { Value = x, Text = $"Item{x}" })
+                .AsQueryable();
+        }
+    }
+}
