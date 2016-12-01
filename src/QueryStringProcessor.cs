d55d36e2b3297a9cc3adc903e6e812fe6e2ecb0f
Jesse <jesse@jesseh.net>
2016-11-30 23:59:11 -0800

    add bracket grouping and simple test

--- src/QueryStringProcessor.cs
+++ src/QueryStringProcessor.cs
@@ -9,26 +9,61 @@ namespace UrlFilter
     {        
         public static Expression<Func<T,bool>> GetWhereExpression(string queryString)
         {
-            var segments = GetWhereSegments(queryString);
-            var paramExpression = Expression.Parameter(typeof(T));
+            var tokens = GetWhereSegments(queryString);
+            var parameterExpression = Expression.Parameter(typeof(T));
 
-            var expression = ReduceQuerySegments(segments, paramExpression);
-
-            return Expression.Lambda<Func<T, bool>>(expression, paramExpression);
+            var expression = ReduceGroupSegments(tokens, parameterExpression);
+            return Expression.Lambda<Func<T, bool>>(expression, parameterExpression);
         }
 
-        private static Expression ReduceQuerySegments(LinkedList<Token> tokens, ParameterExpression paramExpression)
+        private static Expression ReduceGroupSegments(List<Token> tokens, ParameterExpression parameterExpression)
+        {
+            var currentTokens = tokens;
+            while (currentTokens.Count != 1)
+            {
+                currentTokens = ProcessGroupPriority(currentTokens, parameterExpression);
+            }
+            return currentTokens.Single().OperatorExpression;
+        }
+
+        private static List<Token> ProcessGroupPriority(List<Token> tokens, ParameterExpression parameterExpression)
+        {
+            var leftBracket = 0;
+
+            for (int i = 0; i < tokens.Count -1; i++)
+            {
+                var token = tokens[i];
+                if (token.GroupPriority == OperatorPrecedence.Precedence.Grouping)
+                {
+                    if(token.TokenValue == "(")
+                    { 
+                        leftBracket = i;
+                    }
+                    else if(token.TokenValue == ")")
+                    {
+                        var groupTokens = tokens.Skip(leftBracket + 1).Take(i - leftBracket - 1).ToList();
+                        var groupExpression = ReduceExpressionSegments(new LinkedList<Token>(groupTokens), parameterExpression);
+                        tokens.RemoveRange(leftBracket, i - leftBracket + 1);
+                        tokens.Insert(leftBracket, new Token { OperatorExpression = groupExpression });
+                        return tokens;
+                    }
+                }
+            }
+            return new List<Token> { new Token { OperatorExpression = ReduceExpressionSegments(new LinkedList<Token>(tokens), parameterExpression) } };
+        }
+
+        private static Expression ReduceExpressionSegments(LinkedList<Token> tokens, ParameterExpression parameterExpression)
         {
             ExpressionProcessors.ProcessUnary(tokens, OperatorPrecedence.Precedence.Unary);
-            ExpressionProcessors.ProcessEqualityAndRelational<T>(tokens, OperatorPrecedence.Precedence.Relational, paramExpression);
-            ExpressionProcessors.ProcessEqualityAndRelational<T>(tokens, OperatorPrecedence.Precedence.Equality, paramExpression);
+            ExpressionProcessors.ProcessEqualityAndRelational<T>(tokens, OperatorPrecedence.Precedence.Relational, parameterExpression);
+            ExpressionProcessors.ProcessEqualityAndRelational<T>(tokens, OperatorPrecedence.Precedence.Equality, parameterExpression);
             ExpressionProcessors.ProcessConditional(tokens, OperatorPrecedence.Precedence.ConditionalAnd);
             ExpressionProcessors.ProcessConditional(tokens, OperatorPrecedence.Precedence.ConditionalOr);
 
             return tokens.First.Value.OperatorExpression;
         }
 
-        private static LinkedList<Token> GetWhereSegments(string queryString)
+        private static List<Token> GetWhereSegments(string queryString)
         {
             var querySegments = PadBracketsWithSpace(queryString).Split(' ');
             var tokens = querySegments
@@ -37,9 +72,9 @@ namespace UrlFilter
                 {
                     GroupPriority = OperatorPrecedence.GetOperatorPrecedence(x),
                     TokenValue = x
-                });
+                }).ToList();
             
-            return new LinkedList<Token>(tokens);
+            return tokens;
         }
 
         private static string PadBracketsWithSpace(string queryString)
