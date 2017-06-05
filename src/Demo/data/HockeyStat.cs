8f4d9ad23a60ab26eb04b5b97c1270cdbe5350c8
Jesse Hamilton <jesse@jesseh.net>
2017-06-05 10:01:24 -0700

    add hockey stat loader

---
+++ src/Demo/data/HockeyStat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DemoApi.data
+{
+    public class HockeyStat
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string ShootCatch { get; set; }
+        public string Pos { get; set; }
+        public DateTime Birthday { get; set; }
+        public string BirthCountry { get; set; }
+        public int Year { get; set; }
+        public int Stint { get; set; }
+        public string Team { get; set; }
+        public string League { get; set; }
+        public int? GP { get; set; }
+        public int? G { get; set; }
+        public int? A { get; set; }
+        public int? Pts { get; set; }
+        public int? Pim { get; set; }
+        public int? PM { get; set; }
+        public int? PPG { get; set; }
+        public int? PPA { get; set; }
+        public int? SHG { get; set; }
+        public int? SHA { get; set; }
+        public int? GWG { get; set; }
+        public int? GTG { get; set; }
+        public int? SOG { get; set; }
+    }
+}
---
+++ src/Demo/data/Loader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace DemoApi.data
+{
+    public class Loader
+    {
+        public List<HockeyStat> Stats { get; set; }
+
+        public void LoadData()
+        {
+            JsonTextReader reader = new JsonTextReader(new StreamReader("Hockey.json"));
+            Stats = JsonConvert.DeserializeObject<List<HockeyStat>>(File.ReadAllText("Hockey.json"));
+
+        }
+    }
+}
