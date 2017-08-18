de4ff0e29508cefe2b66424fd6e5b784a6587299
Jesse Hamilton <jesse@jesseh.net>
2017-08-18 15:03:07 -0700

    add json and update loader

---
+++ src/Demo/data/HockeyStat.cs
@@ -0,0 +1,44 @@
+﻿namespace DemoApi.data
+{
+    public class HockeyStat
+    {
+        public int assists { get; set; }
+        public float faceoffWinPctg { get; set; }
+        public int gameWinningGoals { get; set; }
+        public int gamesPlayed { get; set; }
+        public int goals { get; set; }
+        public int otGoals { get; set; }
+        public int penaltyMinutes { get; set; }
+        public string playerBirthCity { get; set; }
+        public string playerBirthCountry { get; set; }
+        public int playerBirthDate { get; set; }
+        public string playerBirthStateProvince { get; set; }
+        public int playerDraftOverallPickNo { get; set; }
+        public int playerDraftRoundNo { get; set; }
+        public int playerDraftYear { get; set; }
+        public string playerFirstName { get; set; }
+        public int playerHeight { get; set; }
+        public int playerId { get; set; }
+        public int playerInHockeyHof { get; set; }
+        public string playerLastName { get; set; }
+        public string playerName { get; set; }
+        public string playerNationality { get; set; }
+        public string playerPositionCode { get; set; }
+        public string playerShootsCatches { get; set; }
+        public string playerTeamsPlayedFor { get; set; }
+        public int playerWeight { get; set; }
+        public int plusMinus { get; set; }
+        public int points { get; set; }
+        public float pointsPerGame { get; set; }
+        public int ppGoals { get; set; }
+        public int ppPoints { get; set; }
+        public int seasonId { get; set; }
+        public int shGoals { get; set; }
+        public int shPoints { get; set; }
+        public float shiftsPerGame { get; set; }
+        public float shootingPctg { get; set; }
+        public int shots { get; set; }
+        public float timeOnIcePerGame { get; set; }
+    }
+
+}
--- src/Demo/data/Loader.cs
+++ src/Demo/data/Loader.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Web;
 using Newtonsoft.Json;
 
 namespace DemoApi.data
 {
     public class Loader
     {
-        public List<Episode> Stats { get; set; }
+        public List<HockeyStat> Stats { get; set; }
 
         public void LoadData()
         {
-            Stats = JsonConvert.DeserializeObject<List<Episode>>(File.ReadAllText("SimpsonsEpisodes.json"));
-            
+            Stats = JsonConvert.DeserializeObject<List<HockeyStat>>(File.ReadAllText("NHL2016.json"));            
         }
     }
 }
