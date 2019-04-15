47134f36316e42f0b61d9dcbd53d701990cf2b66
Jesse <jesse@jesseh.net>
2019-04-14 22:02:05 -0700

    more refactoring

--- src/UrlFilter/ExpressionSegment.cs
+++ src/UrlFilter/ExpressionSegment.cs
@@ -4,30 +4,7 @@ namespace UrlFilter
 {
     public class ExpressionSegment
     {
-        public int StartIndex { get; set; }
-        public int EndIndex { get; set; }
-        public bool IsProcessed => Expression != null;
-        public int Length => EndIndex - StartIndex + 1;
+        public string Text { get; set; }
         public Expression Expression { get; set; }
-
-        public bool ContainsEntirely(ExpressionSegment child)
-        {
-            return ContainsEntirely(child.StartIndex, child.EndIndex);
-        }
-
-        public bool ContainsEntirely(int childStartIndex, int childEndIndex)
-        {
-            return StartIndex <= childStartIndex && EndIndex >= childEndIndex;
-        }
-
-        public bool IsLastSegment(string queryString)
-        {
-            return StartIndex == 0 && EndIndex == queryString.Length - 1;
-        }
-
-        public string SegmentText(string queryString)
-        {
-            return queryString.Substring(StartIndex, Length);
-        }
     }
 }
